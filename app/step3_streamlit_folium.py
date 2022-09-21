c3d02f492c83bf9224adeb4d92524a5fef633d16
level-999-mafia-boss <s10242046@connect.np.edu.sg>
2022-09-21 14:55:52 +0800

    change wallpaper

--- app/step3_streamlit_folium.py
+++ app/step3_streamlit_folium.py
@@ -14,7 +14,7 @@ st.set_page_config(
     menu_items={"About": "Data is updated on one minute interval"})
 
 # create a wallpaper in the app
-set_bg("assets/wallpaper.jpg") 
+set_bg("assets/queen_wallpaper.png") 
 
 # create headers and information for the app
 head()
